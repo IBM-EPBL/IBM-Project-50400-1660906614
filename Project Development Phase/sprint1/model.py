6418149f77268c48389e403f570eec3b96dbd64d
Akshaya2411 <65708503+Akshaya2411@users.noreply.github.com>
2022-11-11 12:09:26 +0530

    Update model.py

--- Project Development Phase/sprint1/model.py
+++ Project Development Phase/sprint1/model.py
@@ -1,42 +1,26 @@
-import keras
-from keras.optimizers import Adam, SGD
-import tensorflow as tf
+from keras.optimizers import Adam
 import cv2
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import numpy as np
-from keras import Sequential
+from keras.models import Sequential
 import matplotlib.pyplot as plt
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPool2D
 from keras_preprocessing.image import ImageDataGenerator
-train_path = r'Dataset\test_set'
-test_path = r'Dataset\training_set'
-train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=train_path, target_size=(64,64), class_mode='categorical', batch_size=10,shuffle=True)
-test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_path, target_size=(64,64), class_mode='categorical', batch_size=10, shuffle=True)
-imgs, labels = next(train_batches)
-
+train_path = 'Dataset/test_set'
+test_path = 'Dataset/training_set'
+train=ImageDataGenerator(rescale=1./255,zoom_range=0.2,shear_range=0.2,horizontal_flip=True)
+test=ImageDataGenerator(rescale=1./255)
+train_batches = train.flow_from_directory(directory=train_path, target_size=(64,64), class_mode='categorical', batch_size=300,shuffle=True,color_mode="grayscale")
+test_batches = test.flow_from_directory(directory=test_path, target_size=(64,64), class_mode='categorical', batch_size=300, shuffle=True,color_mode="grayscale")
 model = Sequential()
-
-model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(64,64,3)))
-model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-
-model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding = 'same'))
-model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-
-model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding = 'valid'))
-model.add(MaxPool2D(pool_size=(2, 2), strides=2))
-
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(64,64,1)))
 model.add(Flatten())
-
 model.add(Dense(512,activation ="relu"))
-
 model.add(Dense(9,activation ="softmax"))
-model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
-device_name = tf.test.gpu_device_name()
-if "GPU" not in device_name:
-    history = model.fit(train_batches, batch_size=32,validation_batch_size=32,epochs=5)
-else:
-    with tf.device('/gpu:0'):
-        history = model.fit(train_batches, batch_size=32,validation_batch_size=32,epochs=5)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+history = model.fit(train_batches, batch_size=32,validation_data=test_batches,epochs=5)
+
 model.save('model.h5')
