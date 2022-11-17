dfa21adc16d613e447e6f56e91968881b505a12a
Akshaya2411 <65708503+Akshaya2411@users.noreply.github.com>
2022-11-17 16:31:58 +0530

    Update backend_flask.py

--- Project Development Phase/Sprint 4/Flask Application/backend_flask.py
+++ Project Development Phase/Sprint 4/Flask Application/backend_flask.py
@@ -3,7 +3,6 @@ import os
 import math
 import cv2
 from fer import FER
-import speech_recognition as sr
 import pyttsx3
 from keras.models import model_from_json
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
